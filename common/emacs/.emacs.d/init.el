d010b040490964e1bdd6a210c57ed1f1b47b092f
Emil Lundmark <emil@lndmrk.se>
2018-03-14 16:28:34 +0100

    emacs: move config to org

--- common/emacs/.emacs.d/init.el
+++ common/emacs/.emacs.d/init.el
@@ -1,28 +1,7 @@
+;; Load these settings a.s.a.p.
 (setq inhibit-splash-screen t)
 
-;; Place backup files in its own directory
-(defconst backup-directory (concat user-emacs-directory "backups"))
-(setq backup-directory-alist `((".*" . ,backup-directory)))
-(setq auto-save-file-name-transforms `((".*" ,backup-directory t)))
-
-;; Prevent Custom from modifying init.el
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(load custom-file 'noerror)
-
-(setq column-number-mode t)
-
-(setq show-trailing-whitespace t)
-
-(add-hook 'find-file-hook 'linum-mode)
-
-(show-paren-mode t)
-(setq show-paren-delay 0)
-
-(require 'hl-line)
-(global-hl-line-mode)
-
-(add-hook 'text-mode-hook 'flyspell-mode)
-
+;; Initialize packages
 (require 'package)
 (setq package-enable-at-startup nil)
 ;; By default, HTTPS is not used for ELPA
@@ -34,29 +13,5 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-(use-package fill-column-indicator
-  :ensure t
-  :init (setq fci-rule-column 80)
-        (define-globalized-minor-mode global-fci-mode
-                                      fci-mode (lambda () (fci-mode)))
-  :config (global-fci-mode))
-
-(use-package hl-todo
-  :ensure t
-  :config (global-hl-todo-mode))
-
-(use-package magit
-  :ensure t
-  :config (global-magit-file-mode))
-
-(use-package markdown-mode
-  :ensure t
-  :mode "\\.md\\'"
-  :init (setq markdown-command "pandoc"))
-
-(use-package tango-plus-theme
-  :ensure t
-  :config (load-theme 'tango-plus t))
-
-(setq local-file (expand-file-name "local.el" user-emacs-directory))
-(load local-file 'noerror)
+;; Tangle Elisp from Org files
+(mapc #'org-babel-load-file (directory-files user-emacs-directory t "\\.org$"))
