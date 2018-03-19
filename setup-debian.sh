270bd1b52788e34a1d6803f6122ba999f6cfd64c
Emil Lundmark <emil@lndmrk.se>
2018-03-19 21:39:46 +0100

    setup: use explicit escaping

--- setup-debian.sh
+++ setup-debian.sh
@@ -7,6 +7,6 @@ sudo apt-get update && sudo apt-get install --yes \
   stow
 
 for locale in en_US sv_SE; do
-  sudo sed -i "s/^# *\(${locale}.UTF-8\)/\1/" /etc/locale.gen
+  sudo sed -i "s/^# *\\(${locale}.UTF-8\\)/\\1/" /etc/locale.gen
 done
 sudo locale-gen
