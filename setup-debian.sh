67d581f2491ea5938581ae1bdbcb8f7ef6ef08eb
Emil Lundmark <emil@lndmrk.se>
2018-09-23 21:04:09 +0200

    environment: refactor to bundle

--- setup-debian.sh
+++ setup-debian.sh
@@ -3,10 +3,4 @@ sudo apt-get update && sudo apt-get install --yes \
   emacs \
   git \
   gnupg2 \
-  pandoc \
-  python3
-
-for locale in en_US sv_SE; do
-  sudo sed -i "s/^# *\\(${locale}.UTF-8\\)/\\1/" /etc/locale.gen
-done
-sudo locale-gen
+  pandoc
--- setup-fedora.sh
+++ setup-fedora.sh
@@ -1,8 +1,5 @@
 sudo dnf install --assumeyes \
   emacs \
   git \
-  langpacks-en \
-  langpacks-sv \
   gnupg2 \
-  pandoc \
-  python3
+  pandoc
