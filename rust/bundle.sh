b0f79da91204b0627bc32e4dcdcdc6840d942096
Emil Lundmark <emil@lndmrk.se>
2019-01-14 20:32:21 +0100

    rust: revise setup and install component clippy

--- rust/bundle.sh
+++ rust/bundle.sh
@@ -1,25 +1,25 @@
 setup_rust() {
-    if ! rustup; then
+    if ! rustup --version >/dev/null 2>/dev/null; then
         curl --silent --show-error --fail https://sh.rustup.rs \
             | sh -s -- -y --no-modify-path
         # shellcheck disable=SC1090
         . "${HOME}/.cargo/env"
     fi
 
-    if ! rustfmt; then
-        rustup component add rustfmt-preview
-    fi
-
-    if ! rls; then
-        rustup component add rls-preview rust-analysis rust-src
-    fi
+    rustup component add \
+           clippy \
+           rls \
+           rust-analysis \
+           rust-src \
+           rustfmt
 }
 
 test_rust() {
     cargo --version >/dev/null
     rustc --version >/dev/null
+    rustdoc --version >/dev/null
 
-    rustfmt --version >/dev/null
-
+    cargo clippy --version >/dev/null
     rls --version >/dev/null
+    rustfmt --version >/dev/null
 }
