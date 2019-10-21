06ddf7099cb50e093074c5e04f63fe5f944b384a
Emil Lundmark <emil@lndmrk.se>
2019-10-21 22:32:06 +0200

    environment: Configure profile with profile.d fragments

---
+++ roles/environment/files/profile.d/10-path.sh
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2019 Emil Lundmark <emil@lndmrk.se>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+if [ -d "${HOME}/.local/bin" ]; then
+    export PATH="${HOME}/.local/bin:${PATH}"
+fi
