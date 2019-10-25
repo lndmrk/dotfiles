f23b228c915a4bab29a2dc787a79759f0c7292a2
Emil Lundmark <emil@lndmrk.se>
2019-10-25 09:57:51 +0200

    bash: Split config into bashrc.d fragments

---
+++ roles/bash/files/bashrc.d/10-aliases.sh
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: 2019 Emil Lundmark <emil@lndmrk.se>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+alias diff='diff --color=auto'
+alias e='${EDITOR}'
+alias grep='grep --color=auto'
+alias ll='ls -l --color=auto'
+alias ls='ls --color=auto'
+alias r='gio trash'
+alias vi='${EDITOR}'
+alias vim='${EDITOR}'
---
+++ roles/bash/files/bashrc.d/10-prompt.sh
@@ -0,0 +1,5 @@
+# SPDX-FileCopyrightText: 2019 Emil Lundmark <emil@lndmrk.se>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+PS1='\[\e[37m\]\u@\h \[\e[1m\]\W\[\e[0;37m\]\$\[\e[0m\] '
+PS2='\[\e[37m\]>\[\e[0m\] '
