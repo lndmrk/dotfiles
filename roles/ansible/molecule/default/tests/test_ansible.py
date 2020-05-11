a4e8679fa3c2ed98e8bd66597c15ac627d414590
Emil Lundmark <emil@lndmrk.se>
2020-05-11 23:54:43 +0200

    ansible: Fix lint error

--- roles/ansible/molecule/default/tests/test_ansible.py
+++ roles/ansible/molecule/default/tests/test_ansible.py
@@ -7,5 +7,6 @@ def test_ansible_installed(host):
     assert host.package(name).is_installed
     assert host.exists(name)
 
+
 def test_ansible_dependencies_installed(host):
     assert host.package('python3-netaddr').is_installed
