67d73baf7afcf89180eb01e6e69ebc6cb98c243c
Emil Lundmark <emil@lndmrk.se>
2020-05-23 10:51:35 +0200

    emacs: Use emacsclient with systemd socket activation

--- roles/emacs/files/profile.d/20-emacs.sh
+++ roles/emacs/files/profile.d/20-emacs.sh
@@ -1,5 +1,6 @@
 # SPDX-FileCopyrightText: 2019 Emil Lundmark <emil@lndmrk.se>
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-export EDITOR="emacs --no-window-system"
-export VISUAL="emacs"
+export EMACS_SOCKET_NAME="default"
+export EDITOR="emacsclient"
+export VISUAL="${EDITOR}"
