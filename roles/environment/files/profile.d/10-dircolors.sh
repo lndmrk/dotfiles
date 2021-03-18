ddf5d98daa703c66698983248a2f6b1a59fc9559
Emil Lundmark <emil@lndmrk.se>
2021-03-18 13:21:28 +0100

    environment: Make sure dircolors are set

---
+++ roles/environment/files/profile.d/10-dircolors.sh
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2021 Emil Lundmark <emil@lndmrk.se>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+if [ -z "${LS_COLORS}" ]; then
+  eval "$(dircolors --sh "${HOME}/.dir_colors")"
+fi
