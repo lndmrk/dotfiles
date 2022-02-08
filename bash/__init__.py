10c2f91491d555da0f81e5f2b13116ba06e659fb
Emil Lundmark <emil@lndmrk.se>
2022-02-08 23:09:22 +0100

    bash: Convert to bundle

---
+++ bash/__init__.py
@@ -0,0 +1,22 @@
+# SPDX-FileCopyrightText: 2022 Emil Lundmark <emil@lndmrk.se>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+
+def pre_setup(**kwargs):
+    to_backup = [
+        ".bash_login",
+        ".bash_logout",
+        ".bash_profile",
+        ".bashrc",
+    ]
+    for f in to_backup:
+        f = kwargs["target_directory"].joinpath(f)
+        if f.is_file() and not f.is_symlink():
+            f.rename(f.with_suffix(".bak"))
+
+
+def packages(**kwargs):
+    return {
+        "debian": ["bash"],
+        "fedora": ["bash"],
+    }
