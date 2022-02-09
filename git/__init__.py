b826ad1005e0d118193bcade9a7f14bf647a2481
Emil Lundmark <emil@lndmrk.se>
2022-02-09 21:14:41 +0100

    git: Convert to bundle

---
+++ git/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2022 Emil Lundmark <emil@lndmrk.se>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+
+def packages(**kwargs):
+    return {
+        "debian": ["git"],
+        "fedora": ["git"],
+    }
