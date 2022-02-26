dd78d8deeaa8c8338437e72aaa872dd1ee0bbc81
Emil Lundmark <emil@lndmrk.se>
2022-02-26 14:44:37 +0100

    emacs: Configure EDITOR depending on systemd user instance

--- emacs/config/profile.d/20-emacs.sh
+++ emacs/config/profile.d/20-emacs.sh
@@ -1,6 +1,10 @@
 # SPDX-FileCopyrightText: 2019 Emil Lundmark <emil@lndmrk.se>
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-export EMACS_SOCKET_NAME="default"
-export EDITOR="emacsclient"
+if systemctl --user >/dev/null 2>&1; then
+  export EMACS_SOCKET_NAME="default"
+  export EDITOR="emacsclient"
+else
+  export EDITOR="emacs"
+fi
 export VISUAL="${EDITOR}"
