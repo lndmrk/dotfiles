f79ebb80ce30d4d3080a7f33c0ee9df9a93b491f
Emil Lundmark <emil@lndmrk.se>
2022-02-26 14:47:41 +0100

    gpg: Only set SSH_AUTH_SOCK in systemd user instance

--- gpg/config/profile.d/20-gpg.sh
+++ gpg/config/profile.d/20-gpg.sh
@@ -1,4 +1,6 @@
 # SPDX-FileCopyrightText: 2022 Emil Lundmark <emil@lndmrk.se>
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/gnupg/S.gpg-agent.ssh"
+if systemctl --user >/dev/null 2>&1; then
+  export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/gnupg/S.gpg-agent.ssh"
+fi
