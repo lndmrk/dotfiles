6ab2c284d447b31f671abbdbff8646301a28336d
Emil Lundmark <emil@lndmrk.se>
2022-04-06 00:11:21 +0200

    bash: Re-evaluate environment variable in editor aliases
    The e alias now also supports EDITOR being an alias itself.

--- bash/config/bash/bashrc.d/10-aliases.sh
+++ bash/config/bash/bashrc.d/10-aliases.sh
@@ -2,10 +2,10 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 alias diff='diff --color=auto'
-alias e='${EDITOR}'
+alias e="eval \${EDITOR}"
 alias grep='grep --color=auto'
 alias ll='ls -l --color=auto'
 alias ls='ls --color=auto'
 alias r='gio trash'
-alias vi='${EDITOR}'
-alias vim='${EDITOR}'
+alias vi="\${EDITOR}"
+alias vim="\${EDITOR}"
