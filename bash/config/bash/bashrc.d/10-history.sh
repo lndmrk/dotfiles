98b55bbe0542c8795258359248d8afb590256d70
Emil Lundmark <emil@lndmrk.se>
2022-10-05 20:59:45 +0200

    bash: Configure history

---
+++ bash/config/bash/bashrc.d/10-history.sh
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2022 Emil Lundmark <emil@lndmrk.se>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+HISTCONTROL=ignoreboth
+HISTFILESIZE=64000
+HISTSIZE=64000
+HISTTIMEFORMAT="[%F %T %z] "
