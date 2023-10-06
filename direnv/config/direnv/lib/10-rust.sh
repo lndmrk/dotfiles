d21a8167add79b16b537bcdda321bd1908908e62
Emil Lundmark <emil@lndmrk.se>
2023-10-06 20:23:35 +0200

    direnv: Add layout for rustup

---
+++ direnv/config/direnv/lib/10-rust.sh
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2023 Emil Lundmark <emil@lndmrk.se>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+layout_rustup() {
+  # shellcheck disable=SC1091
+  . "${HOME}/.cargo/env"
+}
