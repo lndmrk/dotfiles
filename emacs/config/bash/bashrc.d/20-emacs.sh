5823bf6f13106a2dcff627b9f6df52dbc9c0413b
Emil Lundmark <emil@lndmrk.se>
2024-03-09 13:53:27 +0100

    emacs: Add vterm

--- emacs/config/bash/bashrc.d/20-emacs.sh
+++ emacs/config/bash/bashrc.d/20-emacs.sh
@@ -6,3 +6,21 @@ if [[ "${COLORTERM}" == truecolor ]]; then
   alias emacs='TERM=xterm-direct emacs'
   alias emacsclient='TERM=xterm-direct emacsclient'
 fi
+
+if [[ "${INSIDE_EMACS}" == vterm ]]; then
+  __vterm_printf(){
+    printf "\e]%s\e\\" "${1}"
+  }
+
+  __vterm_prompt(){
+    PS1="${PS1}$(__vterm_printf "51;A$(whoami)@$(hostname):$(pwd)")"
+  }
+
+  if [[ "$(declare -p PROMPT_COMMAND 2>/dev/null)" =~ "^declare -a" ]]; then
+    # shellcheck disable=2206
+    PROMPT_COMMAND=(${PROMPT_COMMAND[@]} __vterm_prompt)
+  else
+    # shellcheck disable=SC2128,SC2178
+    PROMPT_COMMAND="${PROMPT_COMMAND}; __vterm_prompt"
+  fi
+fi
