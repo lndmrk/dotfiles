faad23564de64323500fe28827e2bd9f17d0c2be
Emil Lundmark <emil@lndmrk.se>
2024-10-02 21:30:32 +0200

    direnv: Add rust layout that installs common tools

--- direnv/config/direnv/lib/10-rust.sh
+++ direnv/config/direnv/lib/10-rust.sh
@@ -5,3 +5,22 @@ layout_rustup() {
   # shellcheck disable=SC1091
   . "${HOME}/.cargo/env"
 }
+
+layout_rust() {
+  layout rustup
+
+  rustup toolchain install stable
+  rustup component add \
+         cargo \
+         clippy \
+         rust-analyzer \
+         rust-docs \
+         rust-std \
+         rustc \
+         rust-src \
+         rustfmt
+
+  rustup toolchain install nightly
+  rustup +nightly component add \
+         miri
+}
