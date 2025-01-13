3e6ff45cbf06e86b7b797b8154596f04b6cda05d
Emil Lundmark <emil@lndmrk.se>
2025-01-13 22:10:14 +0100

    direnv: Convert rust layout into a standalone tool
    It's not possible to export functions: https://direnv.net/#faq

--- direnv/config/direnv/lib/10-rust.sh
+++ direnv/config/direnv/lib/10-rust.sh
@@ -5,22 +5,3 @@ layout_rustup() {
   # shellcheck disable=SC1091
   . "${HOME}/.cargo/env"
 }
-
-layout_rust() {
-  layout rustup
-
-  rustup toolchain install stable
-  rustup component add \
-         cargo \
-         clippy \
-         rust-analyzer \
-         rust-docs \
-         rust-std \
-         rustc \
-         rust-src \
-         rustfmt
-
-  rustup toolchain install nightly
-  rustup +nightly component add \
-         miri
-}
